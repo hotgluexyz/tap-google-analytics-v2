7f42befcc917a7c079438c9204a5a25d260b0193
Otavio Trentin <67597089+otavio-trentin@users.noreply.github.com>
2026-04-28 11:51:04 -0300

    Merge pull request #1 from hotgluexyz/feature/hg-5973
    HG-5973: Handle gateway timeouts and DeadlineExceeded

--- tap_google_analytics/client.py
+++ tap_google_analytics/client.py
@@ -348,6 +348,9 @@ class GoogleAnalyticsStream(Stream):
                     )
                     self.quota_manager.wait_for_quota_reset()
                     continue
+                
+                single_line = " ".join(str(e).split())
+                e.args = (single_line,)
                 raise
 
     @staticmethod
